f886153a63f36d2c27d66ee688a93b2f90609e92
S-Rank Monke <osk1sky@yandex.ru>
2023-09-19 19:46:23 +0300

    eslint init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-console.log("Hello, Hexlet!");
-import _ from "lodash";
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
 console.log(_.last(['one', 'two']));
 
-console.log("testing, but not Rocky"); console.log(333  +   333);
+console.log('testing, but not Rocky'); console.log(333 + 333);
